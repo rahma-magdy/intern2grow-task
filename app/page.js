d5e8c09740b23207c8bcf6fb566923873ab505b9
Rahma Magdy <rahma.magdy1411@gmail.com>
2026-03-05 19:32:35 +0200

    updated the design and fetched 8 products only

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
-export default function Home() { 
+export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
       <Link href="/products" className="text-blue-500 hover:underline">
         View Products
--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -6,7 +6,7 @@ export default async function ProductPage({ params }) {
   const res = await req.json();
   console.log(res);
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center h-screen ">
       <h1 className="text-2xl font-bold mb-4">{res.title}</h1>
 
       <p className="mb-2 text-center p-4 pr-5 max-w-xl mx-auto">
@@ -15,7 +15,10 @@ export default async function ProductPage({ params }) {
       <p className="mb-2">Price: ${res.price}</p>
       <p className="mb-2">Brand: {res.brand}</p>
       <p className="mb-2">Category: {res.category}</p>
-      <Link href="/products" className="text-blue-500 hover:underline">
+      <Link
+        href="/products"
+        className="text-blue-500  rounded-2xl border-2 border-blue-500 px-4 py-2 mt-4"
+      >
         Back to Products
       </Link>
     </div>
